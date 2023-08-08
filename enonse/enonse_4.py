b27dbf407e469e221e9af3de9f9ad7bae8d19407
Zakary JN CHARLES <92060265+zakary26@users.noreply.github.com>
2023-08-07 21:36:30 -0400

    Add files via upload

---
+++ enonse/enonse_4.py
@@ -0,0 +1,16 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nb1=9
+>>> nb2=5
+>>> nb3=nb1+nb2
+>>> print(nb3)
+14
+>>> nb3=nb1-nb2
+>>> print(nb3)
+4
+>>> nb3=nb1*nb2
+>>> print(nb3)
+45
+>>> nb3=nb1/nb2
+>>> print(nb3)
+1.8
---
+++ enonse/enonse_5.py
@@ -0,0 +1,14 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=4
+>>> print(type(a))
+<class 'int'>
+>>> b='kode ak Python'
+>>> print(type(b))
+<class 'str'>
+>>> c=7.7
+>>> print(type(c))
+<class 'float'>
+>>> d=None
+>>> print(type(d))
+<class 'NoneType'>
